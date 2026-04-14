40eda8cd9ae7ca9c0a203962b743ab84c69f4694
fidelisray <arjefidelisraymond@gmail.com>
2026-04-14 14:15:12 +0700

    Pengenalan Tipe data String, number, dan Boolean

--- javascript-basics/script.js
+++ javascript-basics/script.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+/*
+    Tipe Data :
+        - String
+        - Number
+        - Boolean
+*/
+
+let myString = 'z'; // String
+let myString2 = "z"; // String
+
+console.log(myString);
+console.log(myString2);
+console.log(typeof myString);
+console.log(typeof myString2);
+
+let pico = "Pico Margondez"; // String
+let sparky = 'Sparky White'; // String
+
+console.log(pico);
+console.log(sparky);
+console.log(typeof pico);
+console.log(typeof sparky);
+
+let umurPico = 3; // number
+let isSparkyWhite = true; // boolean
+
+console.log(`umurPico : ${umurPico} -> type ${typeof umurPico}`);
+console.log(`isSparkyWhite : ${isSparkyWhite} -> type ${typeof isSparkyWhite}`);
+
+let phi = 3.14; // number
+console.log(`phi : ${phi} -> typeof phi ${typeof phi}`);
