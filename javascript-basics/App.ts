1020bd2877cb68df84c9cb487ccc929b7ac910c3
fidelisray <arjefidelisraymond@gmail.com>
2026-05-03 09:24:48 +0700

    Menambah bagian Operator Aritmatika, pengenalan logical comparison dan bitwise operator

--- javascript-basics/App.ts
+++ javascript-basics/App.ts
@@ -1,3 +1,4 @@
+/*
 const x:number = 50;
 
 console.log(x);
@@ -6,5 +7,9 @@ console.log(x);
     var y:string = 'ini di dalam local scope';
 }
 console.log(y);
+*/
 
+const a:boolean = true;
+const b:boolean = true;
 
+console.log(a && b);
--- javascript-basics/script.js
+++ javascript-basics/script.js
@@ -67,7 +67,10 @@ console.log("What's Pico say?" + " " + myDog.bark());
     Operators
 */
 
-// 1. Mathematical Operators
+/* 
+// 1. Arithmatic Operators
+
+// --- standard arithmatic operator (+, -, *, /)
 
 let thisYear = 2025;
 let myDogAge = thisYear - myDog.birthYear;
@@ -85,3 +88,140 @@ console.log("Buy 2kg Tomatoes, Total Prices : Rp." + twoKiloTomatoes + ".00-");
 
 let halfKiloPotatoes = potatoesPrices / 2;
 console.log("Half kilo Potatoes = Rp." + halfKiloPotatoes + ".00-");
+
+// --- Remainder (%)
+let myNumber1 = 7;
+let myNumber2 = 5;
+console.log(`${myNumber1} % ${myNumber2} = ${myNumber1 % myNumber2}`);
+
+// --- Increment (++)
+console.log(`${myNumber1}++`);
+myNumber1++;
+console.log(`-> ${myNumber1}`);
+
+
+// --- Decrement (--)
+console.log(`${myNumber2}--`);
+myNumber2--;
+console.log(`-> ${myNumber2}`);
+
+// --- Exponentiation Operator (**)
+console.log(`${myNumber1} ** 2 -> ${myNumber1 ** 2}`);
+
+// --- unary plus (+)
+const negNumber = -5;
+const posNumber = 7;
+console.log(`+ ${negNumber} -> ${+negNumber}`);
+console.log(`+ ${posNumber} -> ${+posNumber}`);
+
+// --- unary negation (-)
+console.log(`- ${negNumber} -> ${-negNumber}`);
+console.log(`- ${posNumber} -> ${+posNumber}`);
+*/
+
+
+/* 
+// 2. Logical Operators
+
+// --- and (&&)
+
+let a = true;
+let b = false;
+
+console.log(`a && b = ${a && b}`);
+
+// --- or (||)
+console.log(`a || b = ${a || b}`);
+
+// --- not (!)
+console.log(`a = ${a} -> !a = ${!a}`);
+
+// -- nullish (??) 
+let x = 30;
+let y = -5;
+console.log(`y ?? x -> ${y ?? x}`);
+
+*/
+
+
+// 3. Comparisson Operator
+
+const var1 = 4;
+const var2 = "4"
+const var3 = 40;
+
+// --- equal (==)
+console.log("-------------- equal (==)");
+
+console.log(`${var1} (${typeof var1}) == "${var2}" (${typeof var2}) -> ${var1 == var2}`);
+console.log(`"${var2}" (${typeof var2}) == ${var1} (${typeof var1}) -> ${var2 == var1}`);
+console.log(`${var1} (${typeof var1}) == ${var3} (${typeof var3}) -> ${var1 == var3}`);
+
+// --- strict equal (===)
+console.log("-------------- strict equal (===)");
+
+console.log(`${var1} (${typeof var1}) === "${var2}" (${typeof var2}) -> ${var1 === var2}`);
+console.log(`"${var2}" (${typeof var2}) === ${var1} (${typeof var1}) -> ${var2 === var1}`);
+console.log(`${var1} (${typeof var1}) === ${var3} (${typeof var3}) -> ${var1 === var3}`);
+console.log(`${var1} (${typeof var1}) === ${var1} (${typeof var1}) -> ${var1 === var1}`);
+
+
+// --- not equal (!=)
+console.log("-------------- not equal (!=)");
+
+console.log(`${var1} (${typeof var1}) != "${var2}" (${typeof var2}) -> ${var1 != var2}`);
+console.log(`"${var2}" (${typeof var2}) != ${var1} (${typeof var1}) -> ${var2 != var1}`);
+console.log(`${var1} (${typeof var1}) != ${var3} (${typeof var3}) -> ${var1 != var3}`);
+
+// --- strict not equal (!==)
+console.log("-------------- strict not equal (!==)");
+
+console.log(`${var1} (${typeof var1}) !== "${var2}" (${typeof var2}) -> ${var1 !== var2}`);
+console.log(`"${var2}" (${typeof var2}) !== ${var1} (${typeof var1}) -> ${var2 !== var1}`);
+console.log(`${var1} (${typeof var1}) !== ${var3} (${typeof var3}) -> ${var1 !== var3}`);
+console.log(`${var1} (${typeof var1}) !== ${var1} (${typeof var1}) -> ${var1 !== var1}`);
+
+// --- greater than (>)
+console.log("-------------- greater than (>)");
+
+console.log(`${var1} (${typeof var1}) > "${var2}" (${typeof var2}) -> ${var1 > var2}`);
+console.log(`"${var2}" (${typeof var2}) > ${var1} (${typeof var1}) -> ${var2 > var1}`);
+console.log(`${var1} (${typeof var1}) > ${var3} (${typeof var3}) -> ${var1 > var3}`);
+
+// --- greater than or equal (>=)
+console.log("-------------- greater than or equal (>=)");
+
+console.log(`${var1} (${typeof var1}) >= "${var2}" (${typeof var2}) -> ${var1 >= var2}`);
+console.log(`"${var2}" (${typeof var2}) >= ${var1} (${typeof var1}) -> ${var2 >= var1}`);
+console.log(`${var1} (${typeof var1}) >= ${var3} (${typeof var3}) -> ${var1 >= var3}`);
+
+// --- less than (<)
+console.log("-------------- less than (<)");
+
+console.log(`${var1} (${typeof var1}) < "${var2}" (${typeof var2}) -> ${var1 < var2}`);
+console.log(`"${var2}" (${typeof var2}) < ${var1} (${typeof var1}) -> ${var2 < var1}`);
+console.log(`${var1} (${typeof var1}) < ${var3} (${typeof var3}) -> ${var1 < var3}`);
+
+// --- less than or equal (<=)
+console.log("-------------- less than or equal (<=)");
+
+console.log(`${var1} (${typeof var1}) <= "${var2}" (${typeof var2}) -> ${var1 <= var2}`);
+console.log(`"${var2}" (${typeof var2}) <= ${var1} (${typeof var1}) -> ${var2 <= var1}`);
+console.log(`${var1} (${typeof var1}) <= ${var3} (${typeof var3}) -> ${var1 <= var3}`);
+
+// 4. Bitwise Operator
+
+const myNumber3 = 5;
+const myNumber4 = 15;
+
+// --- Bitwise AND (&)
+console.log(`${myNumber3} & ${myNumber4} -> ${myNumber3 & myNumber4}`);
+
+// --- Bitwise OR (|)
+console.log(`${myNumber3} | ${myNumber4} -> ${myNumber3 | myNumber4}`);
+
+// --- Bitwise NOT (~)
+console.log(`~${myNumber4} -> ${~myNumber4}`);
+
+// --- Bitwise XOR (^)
+console.log(`${myNumber3} ^ ${myNumber4} -> ${myNumber3 ^ myNumber4}`);
